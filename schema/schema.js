5497f107acf1e10229772667ab5d9d9c7f801866
Eric Lau <eric.lau@adaptechsolutions.ca>
2017-05-13 15:26:27 -0700

    first commit

---
+++ schema/schema.js
@@ -0,0 +1,34 @@
+const graphql = require('graphql');
+const fetch = require('isomorphic-fetch');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return fetch(`http://localhost:3000/users/${args.id}`).then(res => res.json())
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
