7dd1838b28e7dc2504a0978e378f402953474584
Eric Lau <eric.lau@adaptechsolutions.ca>
2017-05-14 01:16:31 -0700

    to lecture 23

--- schema/schema.js
+++ schema/schema.js
@@ -5,15 +5,37 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
+  GraphQLList
 } = graphql;
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return fetch(`http://localhost:3000/companies/${parentValue.id}/users`).then(res => res.json())
+      }
+    }
+  })
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
-  }
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return fetch(`http://localhost:3000/companies/${parentValue.companyId}`).then(res => res.json())
+      }
+    }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -25,7 +47,14 @@ const RootQuery = new GraphQLObjectType({
       resolve(parentValue, args) {
         return fetch(`http://localhost:3000/users/${args.id}`).then(res => res.json())
       }
-    }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return fetch(`http://localhost:3000/companies/${args.id}`).then(res => res.json())
+      }
+    },
   }
 });
 
